b3247b2ca19741c77aa54871f9ca985f84a74ff7
Ondřej Januš <janusonx@gmail.com>
2017-02-27 16:54:56 -0500

    Completed second task.

---
+++ src/euler/p002.rs
@@ -0,0 +1,29 @@
+struct Fibonacci {
+    curr: u64,
+    next: u64,
+}
+
+impl Iterator for Fibonacci {
+    type Item = u64;
+    fn next(&mut self) -> Option<u64> {
+        let new_next = self.curr + self.next;
+        
+        self.curr = self.next;
+        self.next = new_next;
+        
+        Some(self.curr)
+    }
+}
+
+pub fn sum_even_fib() ->u64 {
+    return Fibonacci { curr: 1, next: 1}
+        .take_while(|&x| x < 4_000_000)
+        .filter(|x| x % 2 == 0)
+        .sum::<u64>();
+}
+
+#[test]
+fn sum_even_up_to_4_000_000() {
+    assert_eq!(sum_even_fib(), 4613732);
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 mod euler;
 
 use euler::p001::sum_multiples;
+use euler::p002::sum_even_fib;
+
 
 fn main() {
+    println!("{}", sum_even_fib());
     print_results(sum_multiples(1000));
 }
 
