5cbfd9fdccc36da570697ac52e761a8c30618210
Ondřej Januš <janusonx@gmail.com>
2017-02-27 11:14:21 -0500

    Refactored solution of the first problem.

--- src/euler/p001.rs
+++ src/euler/p001.rs
@@ -1,30 +1,21 @@
-use std::collections::HashSet;
-
-pub fn sum_multiples(divisors: Vec<i32>, limit: i32) -> i32 {
-    let mut numbers = HashSet::new();
+pub fn sum_multiples(limit: i32) -> i32 {
+    let mut sum: i32 = 0;
     for i in 1..limit {
-        for divisor in divisors.iter() {
-            if i % divisor == 0 {
-                numbers.insert(i);    
-            }
+        if i % 3 == 0 || i % 5 == 0 {
+            sum += i;
         }
     }
-    return sum(numbers);
-}
-
-fn sum(numbers: HashSet<i32>) -> i32 {
-    //let sum = numbers.iter().fold(0u64, |sum, val| sum += val);
-    let sum = numbers.iter().sum::<i32>();
     return sum;
 }
 
+
 #[test]
-fn sum_of_3_5_multiples_in_10() {
-    assert_eq!(sum_multiples(vec!(3, 5), 10), 23);
+fn sum_up_to_10() {
+    assert_eq!(sum_multiples(10), 23);
 }
 
 #[test]
-fn correct_answer() {
-    assert_eq!(sum_multiples(vec!(3, 5), 1000), 233168);
+fn sum_up_to_1000() {
+    assert_eq!(sum_multiples(1000), 233168);
 }
 
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ mod euler;
 use euler::p001::sum_multiples;
 
 fn main() {
-    print_results(sum_multiples(vec!(3, 5), 1000));
+    print_results(sum_multiples(1000));
 }
 
 fn print_results(sum: i32) {
