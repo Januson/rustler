65d5cc2cbabd0ca889a0972be16d3eadc83626be
Ondřej Januš <janusonx@gmail.com>
2017-03-03 12:49:10 +0100

    Clean up.

--- src/euler/p005.rs
+++ src/euler/p005.rs
@@ -10,22 +10,3 @@ pub fn smallest_multiple(max: u32) -> u32 {
         return i as u32;
     }
 }
-
-fn prime_factors(n: u32) -> Vec<u32> {
-    let mut result = vec![];
-    let mut i = 2;
-    let mut num = n;
-    while num > 1 {
-        while num % i == 0 {
-            result.push(i);
-            num /= i;
-        }
-        i += 1;
-    }
-    result
-}
-
-#[test]
-fn prime_factors_of_four() {
-    assert_eq!(prime_factors(4), [2, 2]);
-}
